5dab8000ab421c70c34cf18aa745d4ad9618f28a
jtm853 <johnny469225@gmail.com>
2025-04-02 15:00:20 -0500

    Development update 1, almost ready to start counting steps

--- pedometer_main/pedometer_main.ino
+++ pedometer_main/pedometer_main.ino
@@ -1,7 +1,14 @@
 #include <Wire.h> // Necessary for I2C communication
+#include <LiquidCrystal.h> // loaded library
+
+LiquidCrystal lcd(10, 8, 5, 4, 3, 2);
 
 int accel = 0x53; // I2C address for this sensor (from data sheet)
 float x, y, z;
+float XYZ;
+
+bool any_peak_detected = false;
+bool first_peak_detected = false;
 
 void setup() {
   Serial.begin(9600);
@@ -10,6 +17,12 @@ void setup() {
   Wire.write(0x2D); // Enable measurement
   Wire.write(8); // Get sample measurement
   Wire.endTransmission();
+
+  lcd.begin(16,2); // Initiate LCD in 16x2 configuration
+  lcd.print("test juice");
+  lcd.setCursor(0,1);
+  lcd.print("test 2");
+
 }
 
 void loop() {
@@ -21,11 +34,17 @@ void loop() {
   y = (Wire.read() | Wire.read() << 8); // Parse y values
   y = (Wire.read() | Wire.read() << 8); // Parse z values
 
-  Serial.print("x = "); // Print values
-  Serial.print(x);
-  Serial.print(", y = ");
-  Serial.print(y);
-  Serial.print(", z = ");
-  Serial.println(z);
+  XYZ = sqrt(pow(x,2) + pow(y,2) + pow(z,2));
+  Serial.println(XYZ);
+
+  // Serial.print("x = "); // Print values
+  // Serial.print(x);
+  // lcd.print(x);
+  // Serial.print(", y = ");
+  // Serial.print(y);
+  // lcd.setCursor(0,1);
+  // lcd.print(y);
+  // Serial.print(", z = ");
+  // Serial.println(z);
   delay(200);
 }
